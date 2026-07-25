c83b28c45132f8185a88064e56046962897f5594
kunal <justimagine127@gmail.com>
2026-07-25 17:09:06 +0530

    model setup

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from rag-chatbot!")
-
-
-if __name__ == "__main__":
-    main()
+from langchain_openai import ChatOpenAI
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+import os
+from dotenv import load_dotenv
+load_dotenv()
+nvidia_api_key = os.environ.get("nvidia_api_key")
+v4_flash = ChatNVIDIA(
+    model="deepseek-ai/deepseek-v4-flash",
+    nvidia_api_key=nvidia_api_key,)
+diffusiongemma = ChatNVIDIA(
+  model="google/diffusiongemma-26b-a4b-it",
+  nvidia_api_key=nvidia_api_key,
+)
+response = v4_flash.invoke("Hello, how are you?")
+print(response.content)
